a77014f458f6632b441349aeb1553f2c93e88d64
Aaron Chris <rooneychris83@gmail.com>
2022-12-14 11:52:12 +0100

    Omo changes no dey show

---
+++ aaron.js
@@ -0,0 +1,4 @@
+
+
+
+const jump = "aaronw"
--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import asyncio
+import os
+from watchfiles import awatch, Change, DefaultFilter, watch
+
+
+
+
+directory = 'C:/Users/Last Hokage/Documents/auto-commit'
+
+class WebFilter(DefaultFilter):
+    allowed_extensions = '.html', '.css', '.js'
+
+    def __call__(self, change: Change, path: str) -> bool:
+        return (
+            super().__call__(change, path) and
+            path.endswith(self.allowed_extensions)
+        )
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    
+    if os.path.isfile(f):
+       print(f)
+
+
+
 def main():
+     for changes in watch(f):
+         print(changes)
+
+ 
+asyncio.run(main())
