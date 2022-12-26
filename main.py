f4cdbf2ceee3c74161262f8e1846d14dfaef0389
Aaron Chris <rooneychris83@gmail.com>
2022-12-26 08:53:26 +0100

    my commit

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ last_trigger_time = time.time()
 def currentBranch():
     res = subprocess.run(['git', 'rev-parse', '--abbrev-ref', "HEAD"], capture_output=True)
     return res.stdout.decode().strip()
-branch
+branch = 
 
 
 class FileEventHandler(FileSystemEventHandler):
