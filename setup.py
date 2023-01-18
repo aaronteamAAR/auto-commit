598858d11d491ef42e2d717c42f87811e9315772
Aaron Chris <69767247+aaronteamAAR@users.noreply.github.com>
2023-01-18 21:38:16 +0000

    made changes to egg files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Autocommit-cli',
+    name='autocommit-cli',
     version='1.1.0',
     packages=find_packages(),
     include_package_data=True,
@@ -10,6 +10,6 @@ setup(
     ],
     entry_points='''
     [console_scripts]
-    autocommit=autocommit.cli:main
+    autocommit=autocommit:main
     '''
 )
