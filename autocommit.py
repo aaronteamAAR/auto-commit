d79a371ed6163e3a95e9e0b5c623628793765825
Aaron Chris <69767247+aaronteamAAR@users.noreply.github.com>
2023-01-21 11:23:51 +0000

    another day

--- autocommit.py
+++ autocommit.py
@@ -1,4 +1,4 @@
-import subprocess, click
+import subprocess, click, random
 import os, time, datetime
 import logging
 from halo import Halo
@@ -167,32 +167,26 @@ def push_type(push):
       subprocess.run(['git', 'push', 'origin', f'{branch}'], check=True)
 
 
+
+
 @click.command()
 @click.option("--num_changes", "-n", type=int, required=True, help="Number of changes to make to occur for each commit session")
 def file_changes(changes):
     num_changes = changes
     change = 0
     changes += 1
-    try:
-        if changes >= num_changes:
-            with open("commit_text.txt", "r") as f:
-                  contents = f.read()
-                    # split the contents into words
-                  words = contents.split()
-                    # create an empty dictionary to store the words
-                  word_count = {}
-                    # iterate through the words and add them to the dictionary
-                  for word in words:
-                        if word in word_count:
-                            word_count[word] += 1
-                        else:
-                            word_count[word] = 1
-                        print(word_count)
+    if changes >= num_changes:     
+        word_dict = {}
+        with open('commit_text.txt', 'r') as file:
+            lines = file.readlines()
+        for line in lines:
+            line = line.strip() # remove trailing whitespaces
+            if line in word_dict:
+                word_dict[line] += 1
+            else:
+                word_dict[line] = 1
 
-    except subprocess.CalledProcessError as e:
-        print(f"Git push error with changes to : {e}, try to resolve this manually")
-        changes = 0
-    
+        commit_word = random.choice(list(word_dict.keys()))
 
 main.add_command(push_type)
 main.add_command(git_init)
